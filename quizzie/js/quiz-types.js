a117f43f9fca188ec7990e2585021f062b25dc97
MattWarren10 <mwarren@harding.edu>
2015-10-09 19:03:13 -0400

    rename app quizzie

---
+++ quizzie/js/quiz-types.js
@@ -0,0 +1,18 @@
+$("#progress-wrap").hide();
+
+function qDetail (name, question, choices, answer) {
+	this.name = name;
+	this.question = question
+	this.choices = choices;
+	this.answer = answer;
+}
+
+
+
+//Change the UI according to each question
+
+
+
+
+
+

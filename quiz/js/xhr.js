a6f6b7d4c68c79edfc8565f347414b168c42aa13
MattWarren10 <mwarren@harding.edu>
2015-10-09 18:52:50 -0400

    insert XMLHttpRequest to get JSON quizzes from data folder. list items with radio boxes and labels nested.

---
+++ quiz/js/xhr.js
@@ -0,0 +1,17 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '../data/quizzes.json');
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    var quizzes = JSON.parse(xhr.responseText);
+    var html = '<ul>';
+    for (var i=0; i<quizzes.length; i += 1) {
+    	html += '<li class="list-item">';
+      html += '<input type="radio" id="choice' + [i] + '">';
+      html += '<label for="choice0">' + employees[i].name + '</label>';
+      html += '</li>';
+    }
+    html += '</ul>';
+    document.getElementByTagName('form').innerHTML = html;
+  }
+};
+xhr.send();

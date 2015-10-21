08877da8d2641fb80382852506e71cb60697a186
MattWarren10 <mwarren@harding.edu>
2015-10-21 12:28:57 -0400

    still processing quizzie

--- quizzie/js/quiz-types.js
+++ quizzie/js/quiz-types.js
@@ -16,34 +16,26 @@ var redButton = document.getElementById('red-button');
 var listItems = document.getElementsByClassName('list-items');
 
 
-
-if ($('input[type=radio]:selected')) {
+function Quiz () {
+	if ($('input[type=radio]:selected')) {
 	var qetQuestions = new XMLHttpRequest();
 	qetQuestions.open('GET', 'data/quizzes.json');
 	qetQuestions.onreadystatechange = function () {
 	  if(qetQuestions.readyState === 4 && qetQuestions.status === 200) {
 	    var quizzes = JSON.parse(qetQuestions.responseText);
 	  	for (i=0; i<quizzes.length; i++) {
-	  		
-
-
 	   	$('h2').text(quizzes[i].questions.question);	
-	    
-
-	   }
-	    
-
-	   
-
-	    
-
-
-	    var html = '<ul>';
+	   	var html = '<ul>';
 	    for (var i=0; i<quizzes.length; i += 1) {
 	    	html += '<li class="list-items">';
-	      html += '<input type="radio" id="choice' + [i] + '">';
+	      html += '<input type="radio" id="choice' + [i] + '" name="choices">';
 	      html += '<label for="choice' + [i] + '">' + quizzes[i].questions.choices + '</label>';
 	      html += '</li>';
+	    }
+
+
+		}
+	    
 	    }
 	    html += '</ul>';
 	    html += '<button id="red-button">SELECT</button>';
@@ -55,10 +47,21 @@ if ($('input[type=radio]:selected')) {
 }
 
  
+ //when a user selects a quiz and clicks #red-button
+ 	//get value of for attr from label
+ 	//the value of the for attr will be the id of the input radio from which we will get the text of
+ 	//the text of the id will point us to which object in JSON to get our questions, question, choices, and answer
+ 	//print question in h2 
+ 	//print choices in each label
+ 	//
+
 
 
   //show the first question of the selected quiz
+  	//change text of #red-button to "NEXT"
     //add percentage to width of #progress-bar
+    //if user answers correct
+    	//add 1 to their score
   //show the next question
     //add percentage to width of #progress-bar
 //on last question
